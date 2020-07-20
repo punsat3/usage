54f29030f4e216ac599ca3a7ce3786272b94329e
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-07-20 14:25:55 +0530

    script with role and sk ak

--- main.py
+++ main.py
@@ -5,60 +5,70 @@ from config import *
 import calendar
 import sys
 import os
+from botocore.exceptions import ClientError
 from dateutil.relativedelta import relativedelta
 
 
 logging.basicConfig(filename='elb.log', level=int(Level),
                     format='%(asctime)s:%(levelname)s:%(message)s',filemode='w')
-
-
+#checking whether account_details
 if len(account_details)==0:
     logging.info("Please Provide at least one account details in config file")
-    logging.debug("Please check account_details in config file")
     sys.exit(0)
+
+# loop is goingon in account_details
 for x in account_details:
-    region="aws configure set default.region ap-south-1"
-    profile="aws configure set profile." + x['Account_Name'] + ".role_arn arn:aws:iam::" + x['Account_ID'] + ":role/s3_cross_account"
-    meta_data="aws configure set profile." + x['Account_Name'] + ".credential_source Ec2InstanceMetadata"
-    os.system(region)
-    os.system(profile)
-    os.system(meta_data)
-    print(region)
-    print(profile)
-    print(meta_data)
-    session=boto3.Session(profile_name=x['Account_Name'])
-    sts = session.client('sts')
+    #try to set profile withRole arn
     try:
+        region="aws configure set default.region us-west-2"
+        profile="aws configure set profile." + x['Account_Name'] + ".role_arn arn:aws:iam::" + x['Account_ID'] + ":role/s3_cross_account"
+        meta_data="aws configure set profile." + x['Account_Name'] + ".credential_source Ec2InstanceMetadata"
+        os.system(region)
+        os.system(profile)
+        os.system(meta_data)
+        session=boto3.Session(profile_name=x['Account_Name'])
+        sts = session.client('sts')
         sts.get_caller_identity()
-    except:
-        logging.info(x['Account_Name']+ " Account invalid. Please configure IAM roles Properly for this account "+x['Account_ID'])
-        print("")
-        continue
+        logging.info(x['Account_Name']+ " account has valid role and successfully login with role  in account  "+x['Account_ID'])
+    except ClientError as e:
+        logging.warning(x['Account_Name']+ " account has invalid Role. Please configure proper IAM roles and now we are trying with  access keys for this account "+x['Account_ID'])
+        logging.debug(x['Account_Name']+ " account has invalid Role. Please configure proper IAM role and policies in account "+x['Account_ID'])
+        try:
+            if e.response['Error']['Code'] == 'AccessDenied':
+                session=boto3.Session(aws_access_key_id=x['access_key'],aws_secret_access_key=x['secret_access_key'])
+                sts = session.client('sts')
+                sts.get_caller_identity()
+                logging.info(x['Account_Name']+ " account has valid accesskey and secret-accesskey and successfully login   in account "+x['Account_ID'])
+        except:
+            logging.info(x['Account_Name']+ " account has invalid Details. Please configure proper IAM roles or access keys for this account "+x['Account_ID'])
+            continue
     #required Variables
-    CURRENT_DAY_DATE = str(datetime.today()).split()[0]
-    DATE_REPLACE = str(datetime.today().replace(day=1))
-    FIRST_DAY_DATE = DATE_REPLACE.split()[0]
-    LAST_DAY_DATE = str(date.today().replace(day=calendar.monthrange(date.today().year, date.today().month)[1]))
-    DATE_ADDING = str(datetime.today() + timedelta(days=1))
-    TOMORROW_DAY_DATE = DATE_ADDING.split()[0]
+    FIRST_DAY_DATE = str(date.today().replace(day=1))
+    TOMORROW_DAY_DATE = str(date.today() + timedelta(days=1))
     NEXT_MONTH_FIRST_DATE = str(date.today().replace(day=1) + relativedelta(months=1))
     NOW = datetime.now()
     MONTH_NAME = NOW.strftime('%B')
+    LAST_DAY_OF_CURRENT_MONTH=calendar.monthrange(date.today().year,date.today().month)[1]
+    TODAY_DATE_IN_DAY=date.today().day
+    #TODAY_DATE_FULL=str(date.today())
 
-    client1 = session.client('ce')
+    ce_client = session.client('ce')
 
-    response1 = client1.get_cost_and_usage(TimePeriod={"Start": FIRST_DAY_DATE,"End": NEXT_MONTH_FIRST_DATE},Granularity='MONTHLY',Metrics=['UNBLENDED_COST',],GroupBy=[{'Type': 'DIMENSION','Key': 'LINKED_ACCOUNT'},],)
-    for groups in response1['ResultsByTime']:
+    cost_usage_response = ce_client.get_cost_and_usage(TimePeriod={"Start": FIRST_DAY_DATE,"End": NEXT_MONTH_FIRST_DATE},Granularity='MONTHLY',Metrics=['UNBLENDED_COST',],GroupBy=[{'Type': 'DIMENSION','Key': 'LINKED_ACCOUNT'},],)
+    for groups in cost_usage_response['ResultsByTime']:
         for amount in groups['Groups']:
           MONTH_TO_DATE_COST=str(amount['Metrics']['UnblendedCost']['Amount'])
-    #Taking projection cost from forecast
-    if TOMORROW_DAY_DATE==NEXT_MONTH_FIRST_DATE:
-      logging.info(str(MONTH_NAME)+" Today is lastday of this month There is nothing predict cost. please check overall cost ")
-        #response2 = client1.get_cost_forecast(TimePeriod={'Start': TOMORROW_DAY_DATE,'End': NEXT_MONTH_FIRST_DATE},Metric='UNBLENDED_COST',Granularity='MONTHLY',PredictionIntervalLevel=99)
+        logging.info("For account "+x['Account_ID']+" With Name "+x['Account_Name']+" :")
+        logging.info("-----------------------------------------")
+        logging.info(str(MONTH_NAME)+" month to date cost for account "+ x['Account_Name'] +" ("+x['Account_ID']+") is : $"+str(MONTH_TO_DATE_COST))
+
+#Taking projection cost from forecast
+
+    if TODAY_DATE_IN_DAY == LAST_DAY_OF_CURRENT_MONTH:
+        logging.info("Today is the last day in this month. and aws not supporting for hourly basis prediction please take manually")
+
     else:
-      response2 = client1.get_cost_forecast(TimePeriod={'Start': TOMORROW_DAY_DATE,'End': NEXT_MONTH_FIRST_DATE},Metric='UNBLENDED_COST',Granularity='MONTHLY',PredictionIntervalLevel=99)
-    PROJECTION_COST=str(response2['Total']['Amount'])
-    logging.info("For account "+x['Account_ID']+" With Name "+x['Account_Name']+" :")
-    logging.info("-----------------------------------------")
-    logging.info(str(MONTH_NAME)+" month to date cost for account "+ x['Account_Name'] +" ("+x['Account_ID']+") is : $"+str(MONTH_TO_DATE_COST))
-    logging.info(str(MONTH_NAME)+" months Projection cost for account "+ x['Account_Name'] +" ("+x['Account_ID']+") is : $"+str(PROJECTION_COST)+" \n  ")
+        forecast_response = ce_client.get_cost_forecast(TimePeriod={'Start': TOMORROW_DAY_DATE,'End': NEXT_MONTH_FIRST_DATE},Metric='UNBLENDED_COST',Granularity='MONTHLY',PredictionIntervalLevel=99)
+        PROJECTION_COST=str(forecast_response['Total']['Amount'])
+        logging.info(str(MONTH_NAME)+" months Projection cost for account "+ x['Account_Name'] +" ("+x['Account_ID']+") is : $"+str(PROJECTION_COST)+" \n  ")
+
