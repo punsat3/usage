8e01322bd95bee137bd8cc0eb32979edaf0d9ce3
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-08-20 17:47:02 +0530

    job21

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 account_details=[{"Account_Name": "Ingestions","Account_ID": "3437654","access_key":"AKIURGGBT47I","secret_access_key":"9sfpiNW/0ZZpXDBbYmT559fNQBHHt","role":"Trusting_Cross_Account_Cost_Explorer"},{"Account_Name": "Prod","Account_ID": "36497220","access_key":"AKIARGBT47I","secret_access_key":"UieM/9sfNQBHHt","role":"dmi_ec2_tags"},{"Account_Name": "QA","Account_ID": "10757276","access_key":"AKIURGGBT47I","secret_access_key":"9sfpiXDBbYmT559fNQBHHt","role":"Trusting_Cross_Account_Cost_Explorer"},{"Account_Name": "Dev","Account_ID": "648418008839","access_key":"AKIURGGBT47I","secret_access_key":"9sfpiNW/0ZZpXDBbYmT559fNQBHHt","role":"Trusting_Cross_Account_Cost_Explorer"}]
 Level=20
-job20
+job21
 #role="cross_ce"
 #s3_bucket="s3://punsat/sat/"
 #s3_bucket="s3://test-cost-demo-987/cost_logs/"
