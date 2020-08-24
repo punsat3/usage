5beb3b0e338108d476b87056735b6afba4846680
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-08-24 22:38:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 variable "aws_access_key"{}
-variable "var.aws_secret_key"{}
+variable "aws_secret_key"{}
 variable "ami_id" {}
 variable "instance_type" {}
 
@@ -18,8 +18,8 @@ variable "tags" {
 
 
 provider "aws" {
-      //  access_key            =   "${var.aws_access_key}"
-     //   secret_key            =   "${var.aws_secret_key}"
+        access_key              =   "${var.aws_access_key}"
+        secret_key              =   "${var.aws_secret_key}"
         region                  =   "${var.region}"
 }
 resource "aws_instance" "my-instance" {
