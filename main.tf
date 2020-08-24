b2b40da89f2068b6f4d2d4e5a621c730535a7cda
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-08-24 13:53:52 +0530

    new file with changes

---
+++ main.tf
@@ -0,0 +1,33 @@
+variable "ami_id" {}
+variable "instance_type" {}
+
+variable "root_vl_type" {}
+variable "root_vl_size" {}
+variable "subnet_id" {}
+variable "number_of_instances" {}
+variable "sgs" {}
+variable "user_data" {}
+variable "tag_name" {}
+variable "key_name" {}
+variable "region" {}
+variable "tags" {
+  default = {}
+}
+
+
+provider "aws" {
+        region                  =   "${var.region}"
+}
+resource "aws_instance" "my-instance" {
+        ami                     =   "${var.ami_id}"
+        instance_type           =   "${var.instance_type}"
+        subnet_id               =   "${var.subnet_id}"
+        user_data               =   "${file(var.user_data)}"
+        root_block_device {
+                volume_type     =   "${var.root_vl_type}"
+                volume_size     =   "${var.root_vl_size}"
+        }
+        tags                    =   "${var.tags}"
+        vpc_security_group_ids  =   "${var.sgs}"
+        key_name                =   "${var.key_name}"
+}
